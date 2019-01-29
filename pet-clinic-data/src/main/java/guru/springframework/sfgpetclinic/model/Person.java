0c35e791a5deb98ede38c60efe83497ac56076d2
afibarra <afidroid@gmail.com>
2019-01-29 13:39:09 -0500

    Issue #10 - Add ID property as Long

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Person.java
@@ -1,6 +1,6 @@
 package guru.springframework.sfgpetclinic.model;
 
-public abstract class Person {
+public abstract class Person extends BaseEntity {
     private String firstName;
     private String lastName;
 
--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
@@ -1,4 +1,13 @@
 package guru.springframework.sfgpetclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
