a3f17ac8088ce24e0b106f5a609a1ba7d6b48ef7
afibarra <afidroid@gmail.com>
2019-02-04 18:59:26 -0500

    Issue #22 - Adding Spring Conf

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/SfgPetClinicApplication.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/SfgPetClinicApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PetClinicApplication {
+public class SfgPetClinicApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(PetClinicApplication.class, args);
+        SpringApplication.run(SfgPetClinicApplication.class, args);
     }
 }
